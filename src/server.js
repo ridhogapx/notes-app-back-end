80489ae9a662c6bd1ff854f94a71148e8dc6c66b
RageNeko26 <rneko2006@gmail.com>
2023-11-11 21:14:47 +0700

    fix: Add CORS configuration in server entry point

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const init = async() => {
   const server = hapi.server({
     port: 3000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes); 
